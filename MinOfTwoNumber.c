a69c863da8ec282134cb71d559390c606a47b77d
Pawan Sharma <68536005+pawansharma111104@users.noreply.github.com>
2023-08-07 23:40:51 +0530

    Create MinOfTwoNumber.c

---
+++ MinOfTwoNumber.c
@@ -0,0 +1,13 @@
+//minimum of two numbers
+#include<stdio.h>
+int main ()
+{
+ int num1,num2;
+ printf("Enter two numbers-->");
+ scanf("%d%d",&num1,&num2);
+ if (num1<num2)
+ printf("%d is minimum",num1);
+ else
+ printf("%d is minimum",num2);
+ return 0;
+}
