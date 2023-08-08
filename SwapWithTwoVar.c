132f25e4fd97c0ae3af6266a24c67ace07b2d9ec
Pawan Sharma <68536005+pawansharma111104@users.noreply.github.com>
2023-08-08 20:57:46 +0530

    Create SwapWithTwoVar.c

---
+++ SwapWithTwoVar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int a,b;
+ printf("Enter the two values");
+ scanf("%d %d",&a,&b);
+ a=a+b;
+ b=a-b;
+ a=a-b;
+ printf("The value of a is %d",a);
+ printf("\nThe value of b is %d",b);
+ return 0;
+}
