fd670fef1fc32639c2181dab7e92f84c9a20ea0c
Pawan Sharma <68536005+pawansharma111104@users.noreply.github.com>
2023-08-14 21:12:43 +0530

    Create Pattern1While.c

---
+++ Pattern1While.c
@@ -0,0 +1,18 @@
+//Print the pattern using while loop
+//Pattern 1
+#include <stdio.h>
+int main()
+{
+char k='A';
+int l,s;
+for(l=1;l<=3;l++)
+{
+ s=1;
+ while(s<=l)
+ {printf("%c ",k++);
+ s++;
+ }
+ printf("\n");
+}
+return 0;
+}
